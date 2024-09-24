64af2865ab0ff59b04a53c0713f4d036c3334a3b
braboxxxz123 <145605009+braboxxxz123@users.noreply.github.com>
2024-09-24 16:09:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Alimentação ',
+    'Como ter uma Alimentação saúdavel',
+    'Evite consumir alimentos industrializados com muito sal (sódio) como hambúrguer, charque, salsicha, lingüiça, presunto, salgadinhos, conservas de vegetais, sopas, molhos e temperos prontos. – Beba pelo menos 2 litros (6 a 8 copos) de água por dia. Dê preferência ao consumo de água nos intervalos das refeições.'
+)
+
+criaCartao(
+    'Rotina de vida',
+    'Como ter uma rotina bem saúdavel',
+    'Beba bastante água. O corpo humano é composto de mais de 70% de água. Mantenha uma alimentação saudável.Cuide bem do seu sono. ...Faça exercícios físicos regularmente. ...Descanse e se distraia. ...Limite o tempo na frente das telas. ...Mantenha relações saudáveis.Faça consultas periódica' 
+
+)
+
+criaCartao(
+    'Academia e bom para saúde',
+    'O que academia traz para as pessoas',
+    'A atividade física reduz o estresse e sintomas de ansiedade, melhora a qualidade do sono, melhora a aprendizagem, reduz sintomas depressivos, previne e diminui a mortalidade por doenças crônicas como pressão alta e diabetes, melhora a força, equilíbrio e flexibilidade, proporciona a socialização e a convivência.'
+)
+
+criaCartao(
+    'fazer academia por bastante tempo faz mal?',
+    'Não,Exagerar nos treinos pode levar à perda de massa magra, contrariando o que se espera da atividade física. Além disso, o desconforto e a dor causados pelo overtraining farão o atleta ter uma queda no desempenho.',
+    ''
+)
